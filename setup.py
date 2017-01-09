03a5ee596539d1d012ac0ff68797646a6e5e79a8
liujinliu <liujinliu@lbesec.com>
2017-01-09 19:20:14 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def _setup():
+    setup(
+        name='squirrel',
+        version='0.0.1',
+        description='active users data manager',
+        author='Jinliu Liu',
+        author_email='liujinliu@lbesec.com',
+        url='',
+        install_requires=['tornado', 'boto3', 'futures',
+                          'MySQLdb'],
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        entry_points={
+            'console_scripts': [
+                'squirrel-start=squirrel.main:main',
+                ]
+            },
+        classifiers=[
+            'Development Status :: 4 - Beta Development Status',
+            'Environment :: Console',
+            'Topic :: Utilities',
+        ],
+    )
+
+
+def main():
+    _setup()
+
+
+if __name__ == '__main__':
+    main()
